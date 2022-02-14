5bb4698306931099a6c754ae179c9ba7304224cc
justineholland <justineholland2@gmail.com>
2022-02-14 17:01:15 -0600

    added first route to home

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+
+export default function Home() {
+  return (
+    <>
+        <main>
+          <h1>Welcome to Whip</h1>
+          <p> what can you make </p>
+        </main>
+    </>
+
+  )
+}
