fd8ec397cea474f3c52c0f8e116ebf0a56f42839
justineholland <justineholland2@gmail.com>
2022-02-18 10:13:28 -0600

    fixed URL

--- src/PantryNewForm.js
+++ src/PantryNewForm.js
@@ -24,7 +24,7 @@ class PantryNewForm extends Component {
 
 	handleSubmit = (e) => {
 		e.preventDefault()
-		fetch(this.state.baseURL , {
+		fetch(this.state.baseURL + "/api/v1/pantrys" , {
 			method: 'POST',
 			body: JSON.stringify({
 				item: this.state.item,
